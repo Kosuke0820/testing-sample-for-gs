2d2c4049351b60fa02654210021e97c157ff28d5
Kosuke Yamashita <yamashita.ksk@gmail.com>
2020-09-06 07:03:11 +0900

    fix: question

--- test/unit/components/TodoCard.spec.js
+++ test/unit/components/TodoCard.spec.js
@@ -1,102 +1,25 @@
-import { shallowMount } from '@vue/test-utils'
-import TodoCard from '~/components/TodoCard'
-
-const todo = {
-  id: '123',
-  title: 'hoge',
-  deadline: '2020-09-01',
-  isDone: false,
-}
-
 describe('TodoCard', () => {
   describe('template', () => {
     describe('.todo-card ', () => {
-      it('todo が完了済みのとき、is-done class が付与されていること', () => {
-        const wrapper = shallowMount(TodoCard, {
-          propsData: {
-            todo: {
-              ...todo,
-              isDone: true,
-            },
-          },
-        })
-        const todoCard = wrapper.find('.todo-card')
-        expect(todoCard.classes()).toContain('is-done')
-      })
+      it('todo が完了済みのとき、is-done class が付与されていること', () => {})
 
-      it('todo が完了済みのとき、is-done class が付与されていないこと', () => {
-        const wrapper = shallowMount(TodoCard, {
-          propsData: {
-            todo: {
-              ...todo,
-              isDone: false,
-            },
-          },
-        })
-        const todoCard = wrapper.find('.todo-card')
-        expect(todoCard.classes()).not.toContain('is-done')
-      })
+      it('todo が完了済みのとき、is-done class が付与されていないこと', () => {})
     })
   })
 
   describe('computed', () => {
     describe('checkboxLabel', () => {
-      it('todo が完了済みのとき、Undone を返すこと', () => {
-        const wrapper = shallowMount(TodoCard, {
-          propsData: {
-            todo: {
-              ...todo,
-              isDone: true,
-            },
-          },
-        })
-        expect(wrapper.vm.checkboxLabel).toBe('Undone')
-      })
+      it('todo が完了済みのとき、Undone を返すこと', () => {})
 
-      it('todo が未完了のとき、Undone を返すこと', () => {
-        const wrapper = shallowMount(TodoCard, {
-          propsData: {
-            todo: {
-              ...todo,
-              isDone: false,
-            },
-          },
-        })
-        expect(wrapper.vm.checkboxLabel).toBe('Done')
-      })
+      it('todo が未完了のとき、Undone を返すこと', () => {})
     })
   })
 
   describe('methods', () => {
     describe('handleChange', () => {
-      const event = {
-        target: {
-          checked: true,
-        },
-      }
+      it('todo.isDone が変更されること', () => {})
 
-      it('todo.isDone が変更されること', () => {
-        const wrapper = shallowMount(TodoCard, {
-          propsData: {
-            todo,
-          },
-        })
-        wrapper.vm.handleChange(event)
-        expect(wrapper.vm.todo.isDone).toBe(true)
-      })
-
-      it('updateDone が emit されること', () => {
-        const wrapper = shallowMount(TodoCard, {
-          propsData: {
-            todo,
-          },
-        })
-        wrapper.vm.handleChange(event)
-        expect(wrapper.emitted('updateDone')[0][0]).toEqual({
-          id: '123',
-          isDone: true,
-        })
-      })
+      it('updateDone が emit されること', () => {})
     })
   })
 })
